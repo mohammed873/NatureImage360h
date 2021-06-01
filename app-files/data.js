d7e3ad1362aa3f8c3977f2be692469ce1e49f6f0
mohammed873 <melhachimi514@gmail.com>
2021-06-01 20:22:51 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app-files/data.js
@@ -0,0 +1,142 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-8hw0clw-360-panorama-miami",
+      "name": "8hw0clw-360-panorama-miami",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.4203044298904661,
+        "pitch": 0.16842132478617167,
+        "fov": 1.3793455132008117
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2592323290011969,
+          "pitch": 0.7208105607729252,
+          "rotation": 7.0685834705770345,
+          "target": "1-tv5h18p6-landscape"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.9087227349646643,
+          "pitch": -0.08700474369327083,
+          "title": "building",
+          "text": "testing&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "1-tv5h18p6-landscape",
+      "name": "tv5h18p6-landscape",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.005438384804596907,
+        "pitch": 0,
+        "fov": 1.3793455132008117
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.976426674232915,
+          "pitch": 0.799701865219987,
+          "rotation": 1.5707963267948966,
+          "target": "2-zd8vd8sm-central-park-path-with-grass-and-trees"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4396771226594849,
+          "pitch": 0.30729451301716004,
+          "title": "house",
+          "text": "this is just a house"
+        }
+      ]
+    },
+    {
+      "id": "2-zd8vd8sm-central-park-path-with-grass-and-trees",
+      "name": "zd8vd8sm-central-park-path-with-grass-and-trees",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1568,
+      "initialViewParameters": {
+        "yaw": 0.0004425311622764383,
+        "pitch": 0,
+        "fov": 1.3793455132008117
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.947515618826877,
+          "pitch": -0.13765028484455222,
+          "rotation": 0,
+          "target": "0-8hw0clw-360-panorama-miami"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.426957885556277,
+          "pitch": -0.07701570592662321,
+          "title": "garden",
+          "text": "the peace garden"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
